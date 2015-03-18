f1296e7d1c6811445fba5357a9d803324a328b92
Kreutzman Emil <kreutze1@niksula.hut.fi>
2015-03-19 01:54:59 +0200

    improved base css resources and added a simple home view

--- katalogen/urls.py
+++ katalogen/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'login_app.views.index', name='login_home'),
 )
