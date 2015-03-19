73803872edc796500a3f7e1d8d62113681a87265
Stikhin Oleg <stikhio1@niksula.hut.fi>
2015-03-19 02:01:50 +0200

    Member database as of today; functionality which allows to update the database

--- table_query_app/models.py
+++ table_query_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Members(models.Model):
+    member_type = models.CharField(max_length=200)
+    given_names = models.CharField(max_length=200)
+    preferred_name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    maiden_name = models.CharField(max_length=200)
+    nickname = models.CharField(max_length=200)
+    birthdate = models.DateTimeField(null=True)
+    student_number = models.CharField(max_length=10)
+    gender = models.CharField(max_length=10)
+    graduated = models.CharField(max_length=10)
+    title = models.CharField(max_length=200)
+    dead = models.CharField(max_length=10)
+    no_publish = models.CharField(max_length=10)
+    username = models.CharField(max_length=10)
--- table_query_app/views.py
+++ table_query_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from table_query_app.models import *
+import csv
+
 
 # Create your views here.
+def import_db(request):
+    filename = 'alltut.csv'
+    reader = csv.reader(open(filename), delimiter=',', quotechar='"')
+
+    for line in reader:
+        if line:
+            tmp = Members.objects.create()
+            tmp.member_type = line[2]
+            tmp.given_names = line[9]
+            tmp.preferred_name = line[10]
+            tmp.surname = line[11]
+            tmp.maiden_name = line[12]
+            tmp.nickname = line[13]
+            #tmp.birthdate = line[14]
+            tmp.student_number = line[15]
+            tmp.gender = line[16]
+            tmp.graduated = line[17]
+            tmp.title = line[20]
+            tmp.dead = line[23]
+            tmp.no_publish = line[25]
+            tmp.username = line[26]
+            tmp.save()
