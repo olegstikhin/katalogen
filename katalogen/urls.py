a51ccc57848cea8858855bb790d486897681f98f
Oleg Stikhin <oleg.stikhin@gmail.com>
2015-08-13 13:13:47 +0300

    new design of the members list and minor fixes

--- katalogen/urls.py
+++ katalogen/urls.py
@@ -12,6 +12,8 @@ urlpatterns = patterns('',
 
     url(r'^$', 'login_app.views.index', name='login_home'),
     url(r'^medlemmar/$', 'homeview_app.views.get_members', name='members'),
+    url(r'^medlem/$', 'table_query_app.views.get_member_info', name='member_info'),
+    url(r'^namn/$', 'table_query_app.views.get_member_name', name='member_name'),
     url(r'^auth/$', 'login_app.views.login_from_link', name='login_from_link'),
     url(r'^login/$', 'login_app.views.login_attempt', name='login_attempt'),
     url(r'^logout/$', 'login_app.views.logout_user', name='logout_user'),
--- table_query_app/views.py
+++ table_query_app/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from table_query_app.models import *
+from django.template import RequestContext
+
 import csv
 
 
 # Create your views here.
-def import_db(request):
+#def import_db(request):
+#    return HttpResponse("Färdigt")
 
+def get_member_info(request):
+    if request.user.is_authenticated():
+        member = Contactinformationtable.objects.get(pk=request.GET['id'])
+        name = member.member_fld.preferredname_fld + " " + member.member_fld.surname_fld
+        address = member.streetaddress_fld + ", " + member.postalcode_fld + " " + member.city_fld
+        cellphone = member.cellphone_fld
+        phone = member.phone_fld
+        email = member.email_fld
+        return render_to_response("memberinfo.html", {'name': name, 'address': address, 'cellphone': cellphone, 'phone': phone, 'email': email, }, context_instance=RequestContext(request))
+    else:
+        return render_to_response("memberinfo.html", {}, context_instance=RequestContext(request))
 
-    return HttpResponse("Färdigt")
+def get_member_name(request):
+    if request.user.is_authenticated():
+        member = Contactinformationtable.objects.get(pk=request.GET['id'])
+        name = member.member_fld.preferredname_fld + " " + member.member_fld.surname_fld
+        return HttpResponse("<a href='#'>" + name + "</a>")
+    else:
+        return HttpResponse("<a href='#'>" + "</a>")
